ff73517af4c387cbc67e35f7ace0d36d645461ca
David Talbot <david.talbot@gmail.com>
2024-08-15 19:09:14 +0100

    Make it clear that the secret key isn't exposed

--- polls_htmx/settings.py
+++ polls_htmx/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!scx$g=v0!ogfxsn4zdbalkd=%r9uu3yt&^m^tg70rt1uuxkww"
+SECRET_KEY = os.environ["DJANGO_POLLS_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
